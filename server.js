8d3660bf3e797708f6f4cdb61d3545be21f54b47
Rutul Patel <rutul-patel@live.com>
2017-04-27 19:34:26 -0700

    Add Views, Remove html

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var exphbs = require('express-handlebars');
 
 var app = express();
 var PORT = 3000;
@@ -10,13 +11,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 //Routes
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.render("index", {});
 });
 
 app.get("/survey", function(req, res) {
-
+    res.render("survey", {});
 });
 
 app.listen(PORT, function(err) {
